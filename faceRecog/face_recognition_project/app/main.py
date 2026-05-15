4d3ee59fa57801c499c598241d35fe5ca85bddcf
Ali Kamrava <kamrava2003@gmail.com>
2026-05-16 00:59:53 +0330

    main modifying

--- faceRecog/face_recognition_project/app/main.py
+++ faceRecog/face_recognition_project/app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
-from app.routers import persons, faces
-from face_recognition_project.app.routers import embeddings
+from .routers import persons, faces, embeddings
 
 app = FastAPI(
     title="Face Recognition API",
     description="API for face recognition and person management",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 app.include_router(persons.router, prefix="/persons", tags=["Persons"])
 app.include_router(faces.router, prefix="/faces", tags=["Faces"])
 app.include_router(embeddings.router, prefix="/recognition", tags=["Recognition"])
+
 
 @app.get("/")
 def read_root():
